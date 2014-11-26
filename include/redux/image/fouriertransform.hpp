b2662d8ad515c8555cb5b9db3eee9b9635c7fe8d
Tomas Hillberg <hillberg@astro.su.se>
2014-11-26 18:25:38 +0100

    Reorganize the Fourier-transform wrapper a bit. Add routines for convolving etc.

--- include/redux/image/fouriertransform.hpp
+++ include/redux/image/fouriertransform.hpp
@@ -21,38 +21,69 @@ namespace redux {
 
         public:
             
-            struct Plan {
+            struct Plan : public std::enable_shared_from_this<const Plan>  {
+                typedef std::shared_ptr<const Plan> Ptr;
+                enum TYPE { R2C=1, C2C } tp;
                 fftw_plan forward_plan, backward_plan;
                 std::vector<size_t> sizes;
-                Plan( const std::vector<size_t>& dims );
-                template <typename ...S> Plan( S ...s ) : Plan({s...}) {}
+                Plan( const std::vector<size_t>& dims, TYPE tp=R2C );
+                //template <typename ...S> Plan( S ...s ) : Plan({s...}) {}
                 ~Plan();
                 void init( void );
-                bool operator<( const Plan& rhs ) const { return (sizes < rhs.sizes); };
+                Ptr shared(void) const { return shared_from_this(); };
+                bool operator<( const Plan& rhs ) const { if(tp == rhs.tp) return (sizes < rhs.sizes); return tp < rhs.tp; };
             };
 
-            static const Plan& getPlan(const std::vector<size_t>& dims);
-            template <typename ...S> static const Plan& getPlan( S ...s ) { return getPlan({s...}); }
+            static Plan::Ptr getPlan(const std::vector<size_t>& dims, Plan::TYPE tp);
+            //template <typename ...S> static const Plan& getPlan( S ...s ) { return getPlan({s...}); }
             
             FourierTransform() {}
 
             template <typename T>
             FourierTransform( const redux::util::Array<T>&, int flags=0);
-
-            template <typename T>
-            redux::util::Array<T> convolve( const redux::util::Array<T>& ) const;
             
             template <typename T>
-            static void normalize( redux::util::Array<T>& );
+            void inv( redux::util::Array<T>& );
+            
+            template <typename T>
+            redux::util::Array<T> correlate( const redux::util::Array<T>& ) const;
+            
+            void autocorrelate( void );
+            redux::util::Array<double> power( void ) const;
+            
+            void convolveInPlace( redux::util::Array<double>& in, int flags=0 ) const;
+            void convolveInPlace( redux::util::Array<complex_t>& in, int flags=0 ) const;
+            void convolveInPlaceHC( redux::util::Array<complex_t>& in, int flags=0 ) const;
+            redux::util::Array<double> convolve( const redux::util::Array<double>& in, int flags=0 ) const;
+            redux::util::Array<complex_t> convolve( const redux::util::Array<complex_t>& in, int flags=0 ) const;
+            
+            //template <typename T> void convolveInPlace( redux::util::Array<T>&, int flags=0 ) const;
+            //template <typename T> redux::util::Array<T> convolve( const redux::util::Array<T>&, int flags=0 ) const;
+            
+            static void normalize( FourierTransform& );
+            void normalize() { normalize(*this); };
 
             template <typename T>
             static void reorder( redux::util::Array<T>& );
 
+            void reorder(void) { reorder(*this); centered = !centered; };
 
+        private:
+            void init( redux::util::Array<double>&);
+            void init( redux::util::Array<complex_t>&);
+
+            Plan::Ptr plan;
+
+            bool centered;
+            bool halfComplex;
+            bool normalized;
+            bool reOrdered;
+            double nInputElements;
+            
         };
        
-        template <>
-        redux::util::Array<double> FourierTransform::convolve( const redux::util::Array<double>& ) const;
+  //      template <>
+  //      redux::util::Array<double> FourierTransform::convolve( const redux::util::Array<double>& ) const;
 
     }   // image
 
--- src/lib/image/fouriertransform.cpp
+++ src/lib/image/fouriertransform.cpp
@@ -5,9 +5,10 @@
 
 using namespace redux::image;
 using namespace redux::util;
+using namespace redux;
 using namespace std;
 
-FourierTransform::Plan::Plan( const std::vector<size_t>& dims ) {
+FourierTransform::Plan::Plan( const std::vector<size_t>& dims, TYPE tp ) : tp(tp) {
     if ( !dims.empty() ) {
         sizes.clear();
         for( auto it: dims ) {
@@ -22,108 +23,344 @@ FourierTransform::Plan::Plan( const std::vector<size_t>& dims ) {
 
 
 FourierTransform::Plan::~Plan() {
-    fftw_destroy_plan(forward_plan);
-    fftw_destroy_plan(backward_plan);
+    if( ! sizes.empty() ) {
+        fftw_destroy_plan(forward_plan);
+        fftw_destroy_plan(backward_plan);
+    }
 }
 
 
 void FourierTransform::Plan::init( void ) {
-    if( sizes.size() == 2 ) {
-        auto in = sharedArray<double>( sizes[0], sizes[1] );
-        auto out = sharedArray<fftw_complex>( sizes[0], sizes[1] / 2 + 1 );
-        forward_plan = fftw_plan_dft_r2c_2d( sizes[0], sizes[1], *in.get(), *out.get(), FFTW_MEASURE );
-        backward_plan = fftw_plan_dft_c2r_2d( sizes[0], sizes[1], *out.get(), *in.get(), FFTW_MEASURE );
-    }
-    else if (sizes.size() == 1) {
-        auto in = sharedArray<double>( sizes[0] );
-        auto out = sharedArray<fftw_complex>( sizes[0] / 2 + 1 );
-        forward_plan = fftw_plan_dft_r2c_1d( sizes[0], in.get(), out.get(), FFTW_MEASURE );
-        backward_plan = fftw_plan_dft_c2r_1d( sizes[0] / 2 + 1, out.get(), in.get(), FFTW_MEASURE );
-    } else throw std::logic_error( "FT::Plan::init() is only implemented for 1/2 dimensions, add more when/if needed: " + printArray( sizes, "dims" ) );
+    if( tp == R2C ) {
+        if( sizes.size() == 2 ) {
+            auto in = sharedArray<double>( sizes[0], sizes[1] );
+            auto out = sharedArray<fftw_complex>( sizes[0], sizes[1] / 2 + 1 );
+            forward_plan = fftw_plan_dft_r2c_2d( sizes[0], sizes[1], *in.get(), *out.get(), FFTW_MEASURE );
+            backward_plan = fftw_plan_dft_c2r_2d( sizes[0], sizes[1], *out.get(), *in.get(), FFTW_MEASURE );
+        }
+        else if (sizes.size() == 1) {
+            auto in = sharedArray<double>( sizes[0] );
+            auto out = sharedArray<fftw_complex>( sizes[0] / 2 + 1 );
+            forward_plan = fftw_plan_dft_r2c_1d( sizes[0], in.get(), out.get(), FFTW_MEASURE );
+            backward_plan = fftw_plan_dft_c2r_1d( sizes[0] / 2 + 1, out.get(), in.get(), FFTW_MEASURE );
+        } else throw std::logic_error( "FT::Plan::init() is only implemented for 1/2 dimensions, add more when/if needed: " + printArray( sizes, "dims" ) );
+    } else if( tp == C2C )  {
+        if( sizes.size() == 2 ) {
+            auto in = sharedArray<fftw_complex>( sizes[0], sizes[1] );
+            auto out = sharedArray<fftw_complex>( sizes[0], sizes[1] );
+            forward_plan = fftw_plan_dft_2d( sizes[0], sizes[1], *in.get(), *out.get(), FFTW_FORWARD, FFTW_MEASURE );
+            backward_plan = fftw_plan_dft_2d( sizes[0], sizes[1], *in.get(), *out.get(), FFTW_BACKWARD, FFTW_MEASURE );
+        }
+        else if (sizes.size() == 1) {
+            auto in = sharedArray<fftw_complex>( sizes[0] );
+            auto out = sharedArray<fftw_complex>( sizes[0] );
+            forward_plan = fftw_plan_dft_1d( sizes[0], in.get(), out.get(), FFTW_FORWARD, FFTW_MEASURE );
+            backward_plan =  fftw_plan_dft_1d( sizes[0], in.get(), out.get(), FFTW_BACKWARD, FFTW_MEASURE );
+        } else throw std::logic_error( "FT::Plan::init() is only implemented for 1/2 dimensions, add more when/if needed: " + printArray( sizes, "dims" ) );
+    } else throw std::logic_error( "FT::Plan::init() unknown tp." );
 }
 
+namespace {
+    struct PlanCompare {
+        bool operator()( const FourierTransform::Plan::Ptr &a, const FourierTransform::Plan::Ptr &b ) const { return ( *a < *b ); }
+    };
+}
 
-const FourierTransform::Plan& FourierTransform::getPlan( const std::vector<size_t>& dims ) {
-    static std::set<Plan> plans;
+FourierTransform::Plan::Ptr FourierTransform::getPlan( const std::vector<size_t>& dims, Plan::TYPE tp ) {
+    static std::map<std::pair<std::vector<size_t>, Plan::TYPE>,Plan::Ptr> plans;
     static mutex mtx;
     unique_lock<mutex> lock( mtx );
-    return *( plans.emplace( dims ).first );
+    auto plan = plans.emplace(make_pair(dims, tp),nullptr);
+    if( plan.second ) { // insertion successful, i.e. new plan -> initialize it
+        plan.first->second.reset( new Plan(dims, tp) );
+    }
+    return plan.first->second;
 }
 
 
 template <typename T>
-FourierTransform::FourierTransform( const Array<T>& rhs, int flags ) {
+FourierTransform::FourierTransform( const Array<T>& rhs, int flags ) : centered(false), halfComplex(true), normalized(false), reOrdered(false) {
 
-    Array<double> tmp;
-    rhs.copy( tmp );
-    vector<size_t> dims = tmp.dimensions();
+    vector<size_t> dims = rhs.dimensions();
     if( dims.empty() ) throw logic_error( "FourierTransform called with no non-trivial dimensions: " + printArray(rhs.dimensions(), "dims") );
     else if( dims.size() > 2 ) throw logic_error( "FourierTransform only supports 1/2 dimensions at the moment: " + printArray(dimensions(), "dims") );
 
-    dims[dims.size()-1] = dims[dims.size()-1]/2 + 1; 
+    Array<double> tmp;
+    rhs.copy( tmp );
     
-    resize( dims );
+    nInputElements = rhs.nElements();
+    
+    if( flags & FT_REORDER ) {
+        reorder(tmp);
+        reOrdered = true;
+    }
+    
+    init(tmp);
+
+    if( flags & FT_NORMALIZE ) {
+       normalize();
+    }
+    
+
+    
+}
+namespace redux { namespace image {
+template <>
+    FourierTransform::FourierTransform( const Array<complex_t>& rhs, int flags ) : centered(false), halfComplex(false), normalized(false), reOrdered(false) {
+
+    vector<size_t> dims = rhs.dimensions();
+    if( dims.empty() ) throw logic_error( "FourierTransform called with no non-trivial dimensions: " + printArray(rhs.dimensions(), "dims") );
+    else if( dims.size() > 2 ) throw logic_error( "FourierTransform only supports 1/2 dimensions at the moment: " + printArray(dimensions(), "dims") );
+    
+    Array<complex_t> tmp;
+    rhs.copy( tmp );
+    
+    nInputElements = rhs.nElements();
 
     if( flags & FT_REORDER ) {
         reorder(tmp);
+        reOrdered = true;
     }
+    
+    init(tmp);
     
     if( flags & FT_NORMALIZE ) {
-        normalize(tmp);
+       normalize();
     }
-    
-    const Plan& plan = getPlan( tmp.dimensions() );
-    fftw_execute_dft_r2c( plan.forward_plan, tmp.ptr(), reinterpret_cast<fftw_complex*>( ptr() ) );
 
 }
+}}
 template FourierTransform::FourierTransform( const Array<int16_t>&, int );
 template FourierTransform::FourierTransform( const Array<int32_t>&, int );
 template FourierTransform::FourierTransform( const Array<float>&, int );
 template FourierTransform::FourierTransform( const Array<double>&, int );
+template FourierTransform::FourierTransform( const Array<complex_t>&, int );
 
+
+void FourierTransform::init( Array<double>& rhs ) {
+
+    vector<size_t> dims = rhs.dimensions();
+    dims.back() >>= 1;      // for r2c transforms, the last dimension has approx. half size =(n/2+1)
+    dims.back() += 1; 
+    
+    resize( dims );
+    plan = getPlan( rhs.dimensions(), Plan::R2C );
+    fftw_execute_dft_r2c( plan->forward_plan, rhs.ptr(), reinterpret_cast<fftw_complex*>( ptr() ) );
+
+}
+
+void FourierTransform::init( Array<complex_t>& rhs ) {
+    
+    resize( rhs.dimensions() );
+    
+    plan = getPlan( rhs.dimensions(), Plan::C2C );
+    fftw_execute_dft( plan->forward_plan, reinterpret_cast<fftw_complex*>( rhs.ptr() ), reinterpret_cast<fftw_complex*>( ptr() ) );
+
+}
 
 template <typename T>
-Array<T> FourierTransform::convolve( const Array<T>& in ) const {
+void FourierTransform::inv( Array<T>& out ) {
+    vector<size_t> dims = dimensions();
+    if(halfComplex) {
+        dims.back() -= 1; 
+        dims.back() <<= 1;      // for r2c transforms, the last dimension has half size (n/2+1)
+    }
+    if( centered ) reorder();   // TBD: temporary fft-copy or reorder twice ?
+    if(halfComplex) {
+        Array<double> tmp(dims);
+        fftw_execute_dft_c2r( plan->backward_plan, reinterpret_cast<fftw_complex*>( ptr() ), tmp.ptr());
+        out.resize( dims );
+        tmp.copy(out);
+    } else {
+        dims.push_back(2);      // add extra dim for real/imaginary
+        Array<double> tmp(dims);
+        out.resize( dims );
+        fftw_execute_dft( plan->backward_plan, reinterpret_cast<fftw_complex*>( ptr() ), reinterpret_cast<fftw_complex*>( tmp.ptr() ));
+        tmp.copy(out);   // TBD: Add extra dimension of size 2, or discard the imaginary part?
+    }
+    
+    if( !normalized ) {
+        out /= out.nElements();
+    }
+    
+/*
+    if( flags & FT_REORDER ) {
+        reorder(rhs);
+    }
+    
+    if( flags & FT_NORMALIZE ) {
+        normalize(rhs);
+    }
+    
+*/
 
+}
+template void FourierTransform::inv( Array<float>& in );
+template void FourierTransform::inv( Array<double>& in );
+template void FourierTransform::inv( Array<complex_t>& in );
+            
+template <typename T>
+Array<T> FourierTransform::correlate( const Array<T>& in ) const {
+    
+    int f(0);
+    if( ! reOrdered ) {    // only reorder inFT if *this is not already reordered
+        f = ~ FT_REORDER;
+    }
+    FourierTransform inFT( in, f );
+    //inFT *= *this;
+    auto it = inFT.begin();
+    for( auto& it2: *this ) *it++ *= conj(it2);
+    
+    Array<T> out( in.dimensions() );
+    inFT.inv(out);
+    reorder(out);
+    
+    return out;
+    
+}
+template Array<float> FourierTransform::correlate( const Array<float>& ) const;
+template Array<double> FourierTransform::correlate( const Array<double>& ) const;
+template Array<complex_t> FourierTransform::correlate( const Array<complex_t>& ) const;
+            
+
+
+void FourierTransform::autocorrelate( void ) {
+    for( auto& it: *this ) it = norm(it);
+}
+
+
+Array<double> FourierTransform::power( void ) const {
+    
+    Array<double> tmp(dimensions());
+    auto it = tmp.begin();
+    for( auto& it2: *this ) *it++ = norm(it2);
+   
+    return tmp;
+    
+}
+
+void FourierTransform::convolveInPlace( Array<double>& in, int flags ) const {
+    
+    int f = flags;
+    if( reOrdered == (f & FT_REORDER)) {    // only reorder inFT if *this is not already reordered
+        f &= ~(f & FT_REORDER);
+    }
+    FourierTransform inFT( in, f );
+    if( centered != inFT.centered ) inFT.reorder();
+    inFT *= *this;
+    if( inFT.centered ) inFT.reorder();
+    fftw_execute_dft_c2r( plan->backward_plan, reinterpret_cast<fftw_complex*>( inFT.ptr() ), in.ptr() );
+    if( (reOrdered == inFT.reOrdered) != (flags & FT_REORDER)) reorder(in);
+
+}
+
+void FourierTransform::convolveInPlace( Array<complex_t>& in, int flags ) const {
+    int f = flags;
+    if( reOrdered == (f & FT_REORDER)) {    // only reorder inFT if *this is not already reordered
+        f &= ~(f & FT_REORDER);
+    }
+    FourierTransform inFT( in, f );
+    if( centered != inFT.centered ) inFT.reorder();
+    inFT *= *this;
+    if( inFT.centered ) inFT.reorder();
+    fftw_execute_dft( plan->backward_plan, reinterpret_cast<fftw_complex*>( inFT.ptr() ), reinterpret_cast<fftw_complex*>( in.ptr() ) );
+    if( (reOrdered == inFT.reOrdered) != (flags & FT_REORDER)) reorder(in);
+}
+
+
+void FourierTransform::convolveInPlaceHC( Array<complex_t>& in, int flags ) const {
+    int f = flags;
+    if( reOrdered == (f & FT_REORDER)) {    // only reorder inFT if *this is not already reordered
+        f &= ~(f & FT_REORDER);
+    }
+    FourierTransform inFT( in, f );
+    if( centered != inFT.centered ) inFT.reorder();
+cout << "FIX MULTIPLICATION" << endl;
+return;
+    inFT *= *this;
+    if( inFT.centered ) inFT.reorder();
+    fftw_execute_dft( plan->backward_plan, reinterpret_cast<fftw_complex*>( inFT.ptr() ), reinterpret_cast<fftw_complex*>( in.ptr() ) );
+    if( (reOrdered == inFT.reOrdered) != (flags & FT_REORDER)) reorder(in);
+
+}
+
+Array<double> FourierTransform::convolve( const Array<double>& in, int flags ) const {
+    
+    auto dims = in.dimensions();
+    Array<double> ret( dims );
+    if( dims == dimensions() ) {    // matching full complex dimensions -> convolving real function with complex -> result will be complex
+        Array<complex_t> tmp( dims );
+        in.copy( tmp );
+        convolveInPlace(tmp,flags);
+        auto it = ret.begin();
+        for( auto& it2: tmp ) *it++ = real(it2);
+    } else {
+        dims.back() >>= 1;      // for r2c transforms, the last dimension has half size =(n/2+1)
+        dims.back() += 1; 
+        if( dims == dimensions() ) {    // matching halfcomplex dimensions -> convolving two real functions
+            in.copy( ret );
+            convolveInPlace(ret,flags);
+        } else cout << "Dims doesn't match real/complex fft, returning empty result !!!"<<printArray(dims,"  dims") << printArray(dimensions(),"  ldims") << endl;
+    }
+
+    return ret;
+    
+}
+
+Array<complex_t> FourierTransform::convolve( const Array<complex_t>& in, int flags ) const {
+
+cout << "cvlv 4" << endl;
+
+    auto dims = in.dimensions();
+    Array<complex_t> ret( dims );
+    if( dims == dimensions() ) {    // matching full complex dimensions -> convolving complex functions
+        convolveInPlace(ret,flags);
+    } else {
+        dims.back() >>= 1;      // for r2c transforms, the last dimension has approx. half size =(n/2+1)
+        dims.back() += 1; 
+        if( dims == dimensions() ) {    // matching halfcomplex dimensions -> convolving half-complex w. full-complex
+            convolveInPlaceHC(ret,flags);
+        } else cout << "Dims doesn't match real/complex fft, returning empty result !!!" <<printArray(dims,"  dims") << printArray(dimensions(),"  ldims") << endl;
+    }
+    return ret;
+
+}
+
+/*
+template <typename T>
+Array<T> FourierTransform::convolve( const Array<T>& in, int flags ) const {
+cout << "cvlv 5  centered " << centered << "  reOrdered " << reOrdered << endl;
     Array<double> tmp;
     in.copy( tmp );
+    convolveInPlace(tmp,flags);
     Array<T> ret( tmp.dimensions() );
-    const Plan& plan = getPlan( tmp.dimensions() );
-    FourierTransform ft( tmp );
-    ft *= *this;
-    fftw_execute_dft_c2r( plan.backward_plan, reinterpret_cast<fftw_complex*>( ft.ptr() ), tmp.ptr() );
     tmp.copy( ret );
-
     return ret;
 }
 
-template <>
-Array<double> FourierTransform::convolve( const Array<double>& in ) const {
-
-    Array<double> ret( in.dimensions() );
-    const Plan& plan = getPlan( in.dimensions() );
-    FourierTransform ft( in );
-    ft *= *this;
-    fftw_execute_dft_c2r( plan.backward_plan, reinterpret_cast<fftw_complex*>( ft.ptr() ), ret.ptr() );
-
-    return ret;
+template <typename T>
+void FourierTransform::convolveInPlace( Array<T>& in, int flags ) const {
+cout << "cvlv 6  centered " << centered << "  reOrdered " << reOrdered << endl;
+    Array<double> tmp;
+    in.copy( tmp );
+    convolveInPlace(tmp,flags);
+    in.resize( tmp.dimensions() );
+    tmp.copy( in );
 }
-template Array<float> FourierTransform::convolve( const Array<float>& in ) const;
-template Array<double> FourierTransform::convolve( const Array<double>& in ) const;
+template Array<float> FourierTransform::convolve( const Array<float>& in, int ) const;
+template Array<int> FourierTransform::convolve( const Array<int>& in, int ) const;
+template void FourierTransform::convolveInPlace( Array<float>& in, int ) const;
+template void FourierTransform::convolveInPlace( Array<int>& in, int ) const;
+*/
+
+void FourierTransform::normalize( FourierTransform& in ) {
+    in *= (1.0/in.nInputElements);
+    in.normalized = true;
+}
 
 template <typename T>
-void FourierTransform::normalize( Array<T>& in ) {
-    double sum = 0;
-    for( auto it: in ) sum += it;
-    in /= (sum/in.nElements());
-}
-template void FourierTransform::normalize( Array<float>& );
-template void FourierTransform::normalize( Array<double>& );
+void FourierTransform::reorder( redux::util::Array<T>& in ) {
 
-
-template <typename T>
-void FourierTransform::reorder( Array<T>& in ) {
-    
     size_t halfY = in.dimSize(0) / 2;
     size_t stride = in.dimSize(1);
     size_t halfX = stride / 2;
@@ -147,7 +384,11 @@ void FourierTransform::reorder( Array<T>& in ) {
         northWest += stride;
     }
     delete[] buf;
+
 }
-template void FourierTransform::reorder( Array<float>& );
-template void FourierTransform::reorder( Array<double>& );
-template void FourierTransform::reorder( Array<complex_t>& );
+template void FourierTransform::reorder( redux::util::Array<float>& in );
+template void FourierTransform::reorder( redux::util::Array<double>& in );
+template void FourierTransform::reorder( redux::util::Array<complex_t>& in );
+
+
+
