842029c45cb0568d26cd98f16f40703148165481
Tomas Hillberg <hillberg@astro.su.se>
2014-12-11 17:12:56 +0100

    Fix Statistics class, add pack/unpack methods

--- include/redux/image/statistics.hpp
+++ include/redux/image/statistics.hpp
@@ -3,6 +3,7 @@
 
 //#include "redux/image/image.hpp"
 #include "redux/util/array.hpp"
+#include "redux/image/fouriertransform.hpp"
 
 namespace redux {
 
@@ -10,16 +11,27 @@ namespace redux {
 
         enum StatType { ST_VALUES=1, ST_RMS, ST_NOISE=4, ST_ALL=7 };
 
-        template <typename T>
-        struct Statistics  {
-            typedef typename std::shared_ptr<Statistics> Ptr;
+        struct Statistics {
+            typedef std::shared_ptr<Statistics> Ptr;
 
-            void getStats(const redux::util::Array<T>& data, int flags=ST_ALL);
-            void getStats(uint32_t borderClip, const redux::util::Array<T>& data, int flags=ST_ALL);
+            Statistics() : clip(-1), cutoff(-1) {}
+            template <typename T> void getMinMaxMean(const redux::util::Array<T>& data);
+            template <typename T> void getRmsStddev(const redux::util::Array<T>& data, double mean);
+            template <typename T> void getNoise(const redux::util::Array<T>& data);
+            void getNoise(const redux::image::FourierTransform& ft);
             
-            T min, max, median;
+            template <typename T> void getStats(const redux::util::Array<T>& data, int flags=ST_ALL);
+            template <typename T> void getStats(uint32_t borderClip, const redux::util::Array<T>& data, int flags=ST_ALL);
+            
+            static size_t size( void );
+            uint64_t pack( char* ) const;
+            uint64_t unpack( const char*, bool );
+
+            int clip;
+            double cutoff;
+            double min, max, median;
             double mean, rms, stddev;
-            double noisePower, noiseRMS;
+            double noise, noiseRMS;
             uint8_t noiseType;              // flag indicating noise statistics (not used atm.)
 
         };
--- src/lib/image/statistics.cpp
+++ src/lib/image/statistics.cpp
@@ -2,7 +2,6 @@
 
 #include "redux/file/fileana.hpp"
 #include "redux/image/utils.hpp"
-#include "redux/image/fouriertransform.hpp"
 #include "redux/util/bitoperations.hpp"
 
 #include <atomic>
@@ -13,10 +12,10 @@ using namespace redux::util;
 using namespace std;
 
 template <typename T>
-void redux::image::Statistics<T>::getStats( const Array<T>& data, int flags ) {
-
-    min = std::numeric_limits<T>::max();
-    max = std::numeric_limits<T>::min();
+void redux::image::Statistics::getMinMaxMean(const redux::util::Array<T>& data) {
+    
+    min = std::numeric_limits<double>::max();
+    max = std::numeric_limits<double>::lowest();
     double sum = 0;
     size_t nElements = data.nElements();
     for( auto & it : data ) {
@@ -26,9 +25,13 @@ void redux::image::Statistics<T>::getStats( const Array<T>& data, int flags ) {
     }
     mean = sum / nElements;
     
-    // TODO: median
-    
-    if( flags & ST_RMS ) {
+}
+
+           
+template <typename T>
+void redux::image::Statistics::getRmsStddev(const redux::util::Array<T>& data, double mean) {
+    size_t nElements = data.nElements();
+    if(nElements > 1) {
         rms = stddev = 0;
         for( auto & it : data ) {
             rms += static_cast<double>( it * it );
@@ -36,63 +39,53 @@ void redux::image::Statistics<T>::getStats( const Array<T>& data, int flags ) {
             stddev += tmp * tmp;
         }
         rms = sqrt( rms / nElements );
-        stddev = sqrt( stddev / nElements );
+        stddev = sqrt( stddev / (nElements-1) );
+    } else if (nElements == 1) {
+        rms = data(0);
+        stddev = 0;
+    }
+}
+
+
+void redux::image::Statistics::getNoise(const redux::image::FourierTransform& ft) {
+    noise = ft.noise(clip, cutoff);
+}
+
+       
+template <typename T>
+void redux::image::Statistics::getNoise(const redux::util::Array<T>& data) {
+    Array<T> tmpImage = data.copy();            // make a deep copy because "apodize" is destructive.
+    apodize( tmpImage, 8 );                     // edge smoothing to reduce the FFT boundary-artifacts
+    FourierTransform ft( tmpImage, FT_NORMALIZE );
+    getNoise(ft);
+}
+
+
+template <typename T>
+void redux::image::Statistics::getStats( const Array<T>& data, int flags ) {
+
+    getMinMaxMean(data);
+    
+    // TODO: median
+    
+    if( flags & ST_RMS ) {
+        getRmsStddev(data,mean);
     }
     
     if( flags & ST_NOISE ) {
-        Array<T> tmpImage = data.copy();    // make a deep copy because "apodize" is destructive.
-        const std::vector<size_t>& dims = tmpImage.dimensions();
-        std::vector<int64_t> first = tmpImage.first();
-        std::vector<int64_t> last = tmpImage.last();
-        size_t nDims = dims.size();
-        for( size_t i = 0; i < nDims; ++i ) {   // take a centered subimage that has "next smaller power of two" size
-            uint32_t dimSize = tmpImage.dimSize( i );
-            uint32_t newSize = nextPowerOfTwo( dimSize ) >> 1;
-            if( newSize < dimSize ) {
-                int diff = dimSize - newSize;
-                first[i] += diff / 2;
-                last[i] -= ( diff - diff / 2 );
-            }
-        }
-        tmpImage.setLimits( first, last );
-        apodize( tmpImage, 8 );                     // edge smoothing to reduce the FFT boundary-artifacts
-
-        // normalize
-        sum = 0;
-        for( auto it : tmpImage ) sum += it;
-        sum /= tmpImage.nElements();
-        tmpImage /= sum;
-
-        FourierTransform ft( tmpImage );
-
-        size_t Ny = ft.dimSize( 0 );
-        size_t Nx = ft.dimSize( 1 );
-        size_t realNx = tmpImage.dimSize( 1 );  // NB: we are using real-to-complex transform from fftw3, so the transform has dimensions (Ny x Nx/2+1)
-        sum = 0;
-        size_t count = 0;
-        double yxRatio = static_cast<double>( Ny ) / realNx;
-        double Nxy4 = realNx * Ny / 4.0;
-        for( size_t x = realNx / 6; x < Nx; ++x ) {         // skip the Nx/6 pixels nearest the edge (fft artifacts)
-            double xx = x * x * yxRatio;
-            for( size_t y = Ny / 6; y < Ny / 2; ++y ) {     // skip the Ny/6 pixels nearest the edge (fft artifacts)
-                if( ( xx + y * y ) > Nxy4 ) {               // skip a region corresponding to the cutoff-frequency (diffraction limit)  TODO: verify the numbers used here
-                    sum += std::norm( ft( y, x ) );         // norm = abs^2
-                    sum += std::norm( ft( Ny-y-1, x ) );
-                    count += 2;
-                }
-            }
-        }
-        noisePower = sqrt( sum / ( count * Ny * Nx ) );
-
+        getNoise(data);
     }
-    //std::cout << "stats0 (" << myID << "):     min = " << min << "  max = " << max << "  mean = " << mean <<  "  rms = " << rms <<  "  stddev = " << stddev << "  pwr = " << noisePower << std::endl;
 
 }
-template void redux::image::Statistics<float>::getStats( const Array<float>&, int );
-template void redux::image::Statistics<int16_t>::getStats( const Array<int16_t>&, int );
+template void redux::image::Statistics::getStats( const Array<float>&, int );
+template void redux::image::Statistics::getStats( const Array<double>&, int );
+template void redux::image::Statistics::getStats( const Array<int16_t>&, int );
+template void redux::image::Statistics::getStats( const Array<int32_t>&, int );
+
+
 
 template <typename T>
-void redux::image::Statistics<T>::getStats( uint32_t borderClip, const Array<T>& data, int flags ) {
+void redux::image::Statistics::getStats( uint32_t borderClip, const Array<T>& data, int flags ) {
 
     if( !borderClip ) {
         getStats( data, flags );
@@ -119,6 +112,55 @@ void redux::image::Statistics<T>::getStats( uint32_t borderClip, const Array<T>&
     getStats(clippedImage,flags);
     
 }
-template void redux::image::Statistics<float>::getStats( uint32_t, const Array<float>&, int );
-template void redux::image::Statistics<double>::getStats( uint32_t, const Array<double>&, int );
+template void redux::image::Statistics::getStats( uint32_t, const Array<float>&, int );
+template void redux::image::Statistics::getStats( uint32_t, const Array<double>&, int );
+template void redux::image::Statistics::getStats( uint32_t, const Array<int16_t>&, int );
+template void redux::image::Statistics::getStats( uint32_t, const Array<int32_t>&, int );
 
+/*
+            int clip;
+            double cutoff;
+            double min, max, median;
+            double mean, rms, stddev;
+            double noise, noiseRMS;
+            uint8_t noiseType;              // flag indicating noise statistics (not used atm.)
+*/
+size_t redux::image::Statistics::size( void ) {
+    return 1 + sizeof(int) + 9*sizeof(double);
+}
+
+
+
+uint64_t redux::image::Statistics::pack( char* ptr ) const {
+    using redux::util::pack;
+    uint64_t count = pack(ptr, clip);
+    count += pack(ptr+count, cutoff);
+    count += pack(ptr+count, min);
+    count += pack(ptr+count, max);
+    count += pack(ptr+count, median);
+    count += pack(ptr+count, mean);
+    count += pack(ptr+count, rms);
+    count += pack(ptr+count, stddev);
+    count += pack(ptr+count, noise);
+    count += pack(ptr+count, noiseRMS);
+    count += pack(ptr+count, noiseType);
+    return count;
+}
+
+
+uint64_t redux::image::Statistics::unpack( const char* ptr, bool swap_endian ) {
+    using redux::util::unpack;
+    uint64_t count = unpack(ptr, clip, swap_endian);
+    count += unpack(ptr+count, cutoff, swap_endian);
+    count += unpack(ptr+count, min, swap_endian);
+    count += unpack(ptr+count, max, swap_endian);
+    count += unpack(ptr+count, median, swap_endian);
+    count += unpack(ptr+count, mean, swap_endian);
+    count += unpack(ptr+count, rms, swap_endian);
+    count += unpack(ptr+count, stddev, swap_endian);
+    count += unpack(ptr+count, noise, swap_endian);
+    count += unpack(ptr+count, noiseRMS, swap_endian);
+    count += unpack(ptr+count, noiseType, swap_endian);
+    return count;
+
+}
