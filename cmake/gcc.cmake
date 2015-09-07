ee1858ff53d970ceb4bfde9ca67e5936ade25792
Tomas Hillberg <hillberg@astro.su.se>
2015-09-07 10:07:52 +0200

    Fix some cmake scripts.

--- cmake/gcc.cmake
+++ cmake/gcc.cmake
@@ -8,14 +8,14 @@ if (GCC_VERSION VERSION_GREATER 4.7 OR GCC_VERSION VERSION_EQUAL 4.7)
     message(STATUS "C++11 activated.")
     add_definitions("-std=gnu++11")
 elseif(GCC_VERSION VERSION_GREATER 4.3 OR GCC_VERSION VERSION_EQUAL 4.3)
-    message(STATUS "C++0x activated. If you get any errors update to a compiler which fully supports C++11")
+    message(STATUS "C++0x activated. If you get any errors, update to a compiler which fully supports C++11")
     add_definitions("-std=gnu++0x")
 else ()
     message(FATAL_ERROR "C++11 needed. Therefore a gcc compiler with a version higher than 4.3 is needed.")   
 endif()
 
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic -Wall -fstrict-aliasing -O3")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS} pedantic -Wall -fstrict-aliasing -O3 -pg -DDEBUG_")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -pedantic -Wall -fstrict-aliasing -O3")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS} -fPIC -pedantic -Wall -fstrict-aliasing -O3 -pg -DDEBUG_")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99 -w -O3")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -std=c99 -w -O3")
