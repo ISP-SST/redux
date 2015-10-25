9d4118e193ae977f54dc2ab7a493cc7ac28d21dd
Tomas Hillberg <hillberg@astro.su.se>
2015-10-25 16:47:03 +0100

    Expand GSL wrapper to also allow for generic argument-pointer.

--- include/redux/util/gsl.hpp
+++ include/redux/util/gsl.hpp
@@ -27,8 +27,8 @@ namespace redux {
             template<typename T, typename U, typename V>
             class gsl_fdf_wrapper : public gsl_multimin_function_fdf {
             public:
-                gsl_fdf_wrapper( size_t dim, const T& f_in, const U& df_in , const V& fdf_in ) :
-                    f_( f_in ), df_( df_in ), fdf_( fdf_in ) {
+                gsl_fdf_wrapper( size_t dim, const T& f_in, const U& df_in , const V& fdf_in, void* params_in=nullptr ) :
+                    f_(f_in), df_(df_in), fdf_(fdf_in), realParams(params_in) {
                     n = dim;
                     f = &gsl_fdf_wrapper::call_f;
                     df = &gsl_fdf_wrapper::call_df;
@@ -39,8 +39,10 @@ namespace redux {
                 const T& f_;
                 const U& df_;
                 const V& fdf_;
+                void* realParams;
                 static double call_f( const gsl_vector* x, void *params ) {
-                    return static_cast<gsl_fdf_wrapper*>( params )->f_( x, nullptr );
+                    gsl_fdf_wrapper* thisPtr = static_cast<gsl_fdf_wrapper*>( params );
+                    return thisPtr->f_( x, thisPtr->realParams );
                 }
                 static void call_df( const gsl_vector* x, void *params, gsl_vector* df ) {
                     static_cast<gsl_fdf_wrapper*>( params )->df_( x, nullptr, df );
