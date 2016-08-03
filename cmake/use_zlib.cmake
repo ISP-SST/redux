c2953e8e79fc2a5cffe1bff6e9f18e092c8227e1
Tomas Hillberg <hillberg@astro.su.se>
2016-08-03 22:38:54 +0200

    Print notification if zlib is missing.

--- cmake/use_zlib.cmake
+++ cmake/use_zlib.cmake
@@ -13,8 +13,6 @@ set( EXT_MINOR_REGEXP "ZLIB_VER_MINOR" )
 set( EXT_PATCH_REGEXP "ZLIB_VER_REVISION" )
 
 
-set( ${EXT_NAME}_HELPTEXT "Try your systems equivalent of \"apt-get install zlib1g-dev\"" )
-
 # Attempt to locate libs/headers automagically
 include("${CMAKE_CURRENT_LIST_DIR}/FindExternal.cmake")
 
