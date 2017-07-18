96b6735fb9160f256ef0c995fafcb8b769ca8c77
Tomas Hillberg <hillberg@astro.su.se>
2017-07-18 21:32:19 +0200

    Don't parse IDL subversion from header, it is not needed and can cause a bug for some versions.

--- cmake/use_idl.cmake
+++ cmake/use_idl.cmake
@@ -3,7 +3,6 @@
 #
 
 set( EXT_NAME "IDL" )
-set( EXT_REQUIRED_VERSION "7.1" )
 #set( EXT_COMPONENTS idl )
 
 set( EXT_HINT "/usr/local/harris/idl/external/include/"
@@ -18,7 +17,6 @@ set( EXT_HEADER_FILE "idl_export.h" )
 set( EXT_VERSION_FILE "idl_export.h" )
 set( EXT_MAJOR_REGEXP "VERSION_MAJOR" )
 set( EXT_MINOR_REGEXP "VERSION_MINOR" )
-set( EXT_PATCH_REGEXP "VERSION_SUB" )
 
 
 # Attempt to locate libs/headers automagically
@@ -32,7 +30,6 @@ if( NOT DEFINED IDL_FOUND )     # no system installation found, use the header i
     set( EXT_VERSION_FILE "idl_export.h" )
     set( EXT_MAJOR_REGEXP "VERSION_MAJOR" )
     set( EXT_MINOR_REGEXP "VERSION_MINOR" )
-    set( EXT_PATCH_REGEXP "VERSION_SUB" )
     include( "${CMAKE_CURRENT_LIST_DIR}/FindExternal.cmake" )
     if( DEFINED IDL_FOUND )
         set( IDL_RDX 1 CACHE INTERNAL "" FORCE )
