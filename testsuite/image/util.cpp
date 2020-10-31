0736cc5d7dfe3229fb52d07154f5b1c97b168475
Tomas Hillberg <hillberg@astro.su.se>
2020-10-31 09:49:16 +0100

    Add unit test for fitPlane w. masking.

--- testsuite/image/util.cpp
+++ testsuite/image/util.cpp
@@ -17,6 +17,7 @@ namespace testsuite {
 
         void test_plane( void ) {
             
+            double EPS = 1E-5;
             size_t pSize(100);
             double coeffs[3] = { 0, 0, 0 };
             double fit_coeffs[3] = { 0, 0, 0 };
@@ -29,9 +30,9 @@ namespace testsuite {
                 makePlane( plane, coeffs, 1 );
                 // fit and compare
                 fitPlane( plane.ptr(), pSize, pSize, fit_coeffs, &chisq );
-                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], 0.0000001 );
-                BOOST_CHECK_SMALL( fit_coeffs[1], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[2], pSize/2, 0.0000001 );
+                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], EPS );
+                BOOST_CHECK_SMALL( fit_coeffs[1], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[2], pSize/2, EPS );
 
                 // make a plane with slope in x-direction and constant in y.
                 coeffs[0] = 0;
@@ -39,18 +40,18 @@ namespace testsuite {
                 makePlane( plane, coeffs, 2 );
                 // fit and compare
                 fitPlane( plane.ptr(), pSize, pSize, fit_coeffs, &chisq );
-                BOOST_CHECK_SMALL( fit_coeffs[0], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[2], pSize/2, 0.0000001 );
+                BOOST_CHECK_SMALL( fit_coeffs[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[2], pSize/2, EPS );
                 
                 // make a plane with slope in both x/y.
                 coeffs[0] = 1;
                 makePlane( plane, coeffs, 2 );
                 // fit and compare
                 fitPlane( plane.ptr(), pSize, pSize, fit_coeffs, &chisq );
-                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[2], pSize, 0.0000001 );
+                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[2], pSize, EPS );
                 
                 // make a plane with slope in both x/y and a specified mean value.
                 coeffs[0] = 3.333;
@@ -59,34 +60,65 @@ namespace testsuite {
                 makePlane( plane, coeffs, 3 );
                 // fit and compare
                 fitPlane( plane.ptr(), pSize, pSize, fit_coeffs, &chisq );
-                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[2], coeffs[2], 0.0000001 );
+                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[2], coeffs[2], EPS );
                 
                 // Check that the version returning a plane also gives right results.
                 double fit_coeffs2[3] = { 0, 0, 0 };
                 Array<float> plane2 = fitPlane( plane, false, fit_coeffs2, &chisq );
-                BOOST_CHECK_CLOSE( fit_coeffs2[0], coeffs[0], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs2[1], coeffs[1], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs2[2], coeffs[2], 0.0000001 );
+                BOOST_CHECK_CLOSE( fit_coeffs2[0], coeffs[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs2[1], coeffs[1], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs2[2], coeffs[2], EPS );
  
                // Verification that the returned plane is accurate
                 fitPlane( plane2.ptr(), pSize, pSize, fit_coeffs, &chisq );
-                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[2], coeffs[2], 0.0000001 );
+                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[2], coeffs[2], EPS );
                 
                 // same, but subtract average.
                 plane2 = fitPlane( plane, true, fit_coeffs2, &chisq );
-                BOOST_CHECK_CLOSE( fit_coeffs2[0], coeffs[0], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs2[1], coeffs[1], 0.0000001 );
-                BOOST_CHECK_SMALL( fit_coeffs2[2], 0.0000001 );
+                BOOST_CHECK_CLOSE( fit_coeffs2[0], coeffs[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs2[1], coeffs[1], EPS );
+                BOOST_CHECK_SMALL( fit_coeffs2[2], EPS );
                 
                 // Verification that the returned plane is accurate
                 fitPlane( plane2.ptr(), pSize, pSize, fit_coeffs, &chisq );
-                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], 0.0000001 );
-                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], 0.0000001 );
-                BOOST_CHECK_SMALL( fit_coeffs[2], 0.0000001 );
+                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], EPS );
+                BOOST_CHECK_SMALL( fit_coeffs[2], EPS );
+                
+                // 2 superposed planes, separated by masking
+                double coeffs2[3] = { -1.23, 9.45, -23.456 };
+                makePlane( plane2, coeffs2, 3 );
+                size_t nPixels = pSize*pSize;
+                float* pPtr = plane.ptr();
+                float* pPtr2 = plane2.ptr();
+                Array<bool> mask1(pSize,pSize);
+                Array<float> mask2(pSize,pSize);
+                bool* mPtr1 = mask1.ptr();
+                float* mPtr2 = mask2.ptr();
+                for( size_t i(0); i<nPixels; ++i ) {
+                    if( i%4 ) {     // select some subset to be replaced with plane2
+                        pPtr[i] = pPtr2[i];
+                        mPtr2[i] = 1.0;
+                        mPtr1[i] = false;
+                    } else {
+                        mPtr2[i] = 0.0;
+                        mPtr1[i] = true;
+                    }
+                }
+                // fit using mask1, to get plane
+                fitPlane( pPtr, pSize, pSize, mPtr1, fit_coeffs, &chisq );
+                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs[1], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[2], coeffs[2], EPS );
+                // fit using mask2, to get plane
+                fitPlane( pPtr, pSize, pSize, mPtr2, fit_coeffs, &chisq );
+                BOOST_CHECK_CLOSE( fit_coeffs[0], coeffs2[0], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[1], coeffs2[1], EPS );
+                BOOST_CHECK_CLOSE( fit_coeffs[2], coeffs2[2], EPS );
                 
             }
 
